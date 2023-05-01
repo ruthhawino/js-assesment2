348550d617e8c8065d1e69809def210824398614
ruthh awino <awinoruth@gmail.com>
2023-05-01 09:04:18 +0300

    first commit

---
+++ asses.js
@@ -0,0 +1,49 @@
+
+function getAvailableBooks(){
+
+}
+constbooks =[
+{title:'The catch i the eye',author:'J.D Salinger',publicationYear:1951,isAvailable:true},
+{ title:'Great Expectations',author:'Charles Dickens',publicationYear:1861,isAvailable: false },
+{ title:'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:1866, isAvailable:true },
+{ title: 'The Great Gatsby', author:'F.Scott Fitzgerald',publicationYear:1925,isAvailable: false},
+{title:'The Odyssey',author:Homer,publicationYear:-800,isAvailable:true}
+];
+
+constbooks = books.Splice(books=>getAvailableBooks===isAvailable)
+    return getAvailableBooks
+
+
+
+//2. Create a function getBooksByAuthor that takes an author's name as an argument and
+//eturns an array of all books by that author.
+
+function getBooksByauthor(authorsNames){
+    
+}
+(books => books.author === "J.D Salinger","Charlles Dickens","Fyodor Dostoevsky","F.Scott Fitzgerald");
+
+
+//3. Create a function addNewBook that takes a book object as an argument and adds it
+//to the library, ensuring that the new book has all required properties (title, author,
+//publicationYear, and isAvailable).
+
+function addNewBook(title){
+let books = {
+    "title": "Born a crime",
+    "author": "Trevor Noah",
+    "publication year": 1968,
+    "isAvailable":true
+  }
+}
+  books.unshift(books);
+
+ // 4. Create a function checkoutBook that takes a book title as an argument and changes
+//the book's isAvailable property to false. If the book is not found in the library, the
+//function should return a message indicating that the book is not available.
+
+function checkoutBook(title){
+
+    checkoutBook(books=>books.author==="Charle Dickens" && books !=books.isAvailable===false)
+    print("The book title is not available")
+}
